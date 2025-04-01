18d915dc9710d0ae29dc8043f261b4782bdf9cdb
SylviaRemington <sylviaremington@gmail.com>
2025-03-31 22:58:36 -0700

    Exercise 2 completed

--- app.js
+++ app.js
@@ -14,6 +14,7 @@ Below is what I created.)
 // put the next exercise #? 
 // That would make it easier for each subsequent exercise #. Need to figure this out.
 
+//EXERCISE 1 ANSWER
 function maxOfTwoNumbers (num1, num2) {
     if (num1 >= num2){
         return num1;
@@ -23,3 +24,26 @@ function maxOfTwoNumbers (num1, num2) {
 }
 
 console.log ('Exercise 1 Result:', maxOfTwoNumbers(3, 9));
+
+
+/*
+Exercise 2: isAdult()
+
+Write a function named isAdult. It should take an age (number) 
+and return 'Adult' if the age is 18 or over and 'Minor' otherwise.
+
+Example: isAdult(21) should return 'Adult'.
+
+Complete the exercise in the space below:
+*/
+
+//EXERCISE 2 ANSWER - using numbers related to United States rules
+function isAdult(ageInUsa){
+    if (ageInUsa >= 21) {
+        return 'isAdult';
+    } else {
+        return 'isMinor';
+    }
+}
+
+console.log('Exercise 2 Result:', isAdult(21));
